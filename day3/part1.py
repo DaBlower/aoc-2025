c28dbe8c34af4179dd385fada87e5f65944c2976
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-12-03 19:51:31 +1100

    part 1 done

--- day3/part1.py
+++ day3/part1.py
@@ -6,22 +6,28 @@ joltages = 0
 for line in inp:
     highest = 0
     highest_index = 0
-    second_highest = 0
-
     current_index = 0
+    # first digit
     for battery in list(line):
-        if int(battery) > highest:
+        if current_index == len(list(line)) - 1:
+            break
+        elif int(battery) > highest:
             highest = int(battery)
             highest_index = current_index
         current_index += 1
     
+    # second digit
+    second_highest = 0
     current_index = 0
     for battery in list(line):
-        if int(battery) > second_highest and current_index > highest_index:
+        if current_index <= highest_index:
+            current_index += 1
+            continue
+        elif int(battery) > second_highest:
             second_highest = int(battery)
         current_index += 1
-    
-    print(int(str(highest)+str(second_highest)))
+        
+
     joltages += int(str(highest)+str(second_highest))
 
 print(joltages)
